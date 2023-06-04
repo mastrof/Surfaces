983cbb6f746420e7c41a260bf2e17a6193ce185c
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-06-04 10:58:00 +0200

    back to distributed with stricter memory management

--- scripts/msd.jl
+++ scripts/msd.jl
@@ -1,31 +1,38 @@
 ## Libraries
-using DrWatson
-@quickactivate :Surfaces
-using JLD2, DelimitedFiles
-using MeanSquaredDisplacement
-using MicrobeAgents: vectorize_adf_measurement, unfold
-using Base.Threads
+using Distributed
+@everywhere using DrWatson
+@everywhere @quickactivate :Surfaces
+@everywhere begin
+    using JLD2, DelimitedFiles
+    using MeanSquaredDisplacement
+    using MicrobeAgents: vectorize_adf_measurement, unfold
+end
 
 ## Functions to evaluate MSD
-function get_emsd(traj::AbstractMatrix{<:NTuple{2}})
+@everywhere function get_emsd(traj::AbstractMatrix{<:NTuple{2}})
     x = first.(traj)
     y = last.(traj)
     emx = emsd(x)
+    x = nothing
     emy = emsd(y)
+    y = nothing
     [emx emy]
 end
 
-function get_emsd(traj::AbstractMatrix{<:NTuple{3}})
+@everywhere function get_emsd(traj::AbstractMatrix{<:NTuple{3}})
     x = first.(traj)
     y = map(s -> s[2], traj)
     z = last.(traj)
     emx = emsd(x)
+    x = nothing
     emy = emsd(y)
+    y = nothing
     emz = emsd(z)
+    z = nothing
     [emx emy emz]
 end
 
-function get_emsd(fname::AbstractString)
+@everywhere function get_emsd(fname::AbstractString)
     params = parse_savename(fname)[2]
     fout = datadir("proc", savename("emsd", params))
     isfile(fout) && return nothing
@@ -33,14 +40,16 @@ function get_emsd(fname::AbstractString)
     df = simdata["adf"]
     close(simdata)
     traj = unfold(vectorize_adf_measurement(df, :pos), params["L"])
+    df = nothing
     eMSD = get_emsd(traj)
+    traj = nothing
     Δt = 0.01
     t = (axes(eMSD,1).-1) .* Δt
     writedlm(fout, [t eMSD])
+    GC.gc()
+    return nothing
 end
 
 ## Analysis
 filenames = readdir(datadir("sims"))
-@threads for fname in filenames
-    get_emsd(fname)
-end
+pmap(get_emsd, filenames)
