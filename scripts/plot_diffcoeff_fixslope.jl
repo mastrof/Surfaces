216b64300cf3eebe07d405d8dd5e27eb972508b0
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-09-14 15:16:41 +0200

    update plot script

--- scripts/plot_diffcoeff_fixslope.jl
+++ scripts/plot_diffcoeff_fixslope.jl
@@ -12,27 +12,31 @@ Plots.default(
 )
 
 ##
-function makeDplot_cylinders(df, dim, interaction, Drot)
+function makeDplot_cylinders(df, dim, interaction, Drot, mot)
     flt = [
         :dim => d -> d .== dim,
         :interaction => inter -> inter .== interaction,
         :Drot => Dr -> Dr .== Drot,
+        :motilepattern => m -> m .== mot
     ]
     subdf = subset(df, flt)
     gdf = groupby(subdf, [:R])
 
     cmap = palette(:matter, length(gdf))
-    ylab = L"\mathrm{D_{\parallel} \, / \, (U^2 T_s)}"
-    xlab = L"\mathrm{\tau \, / \, T_s}"
+    ylab = L"\mathrm{D_{\parallel} \, / \, (U W)}"
+    xlab = L"\mathrm{U \tau \, / \, W}"
     plot(palette=cmap, xlab=xlab, ylab=ylab, xscale=:log10,
-        leg=:topleft, legendtitle="φ",
-        title="$(dim)d, $(interaction), DᵣTₛ=$(Drot)", titlefontsize=10
+        leg=:topleft, legendtitle="R",
+        title="$(mot) $(dim)d, $(interaction), DᵣW/U=$(Drot)", titlefontsize=10
     )
+    τᵣ = 1 / (2*Drot)
+    Dtheo(τ) = (Drot == 0 ? τ : (τ*τᵣ) / (τ + τᵣ)) / dim
+    #plot!(range(0.1,5,length=500), Dtheo, lc=:green, lw=2, lab=false)
     for g in gdf
         τ = 1 ./ g.λ
         D = (g.Dx + g.Dy)/2
-        φ = round(π*g.R[1]^2; sigdigits=2)
-        plot!(τ, D, m=:c, ms=6, msw=0, lab=φ)
+        R = g.R[1]
+        plot!(τ, D, m=:c, ms=6, msw=0, lab=R)
     end
     plot!()
 end
@@ -44,14 +48,58 @@ df = CSV.read(fname, DataFrame)
 sort!(df, [:λ, :R, :Drot, :dim])
 
 ##
-for dim in [2,3], interaction in ["stick", "slide"], Drot in [0,1]
-    plt = makeDplot_cylinders(df, dim, interaction, Drot)
+for dim in [2,3], interaction in ["stick", "slide"], Drot in [0,1], mot in ["RunTumble"]
+    τ = range(0.1, 15, length=500)
+    plt = makeDplot_cylinders(df, dim, interaction, Drot, mot)
     if Drot == 0
+        D = τ / dim
+        plot!(τ, D, lc=:green, lw=2, alpha=0.5, lab=false)
         plot!(plt, ylims=(0.01, 0.45))
     else
+        #τᵣ = 1 / (2*Drot)
+        #D = @. τ * τᵣ / (dim * (τ + τᵣ))
+        D = @. (1 / dim) * (1 / (1/τ + 2*Drot))
+        plot!(τ, D, lc=:green, lw=2, alpha=0.5, lab=false)
         plot!(plt, ylims=(0.01, 0.175))
     end
-    config = @strdict type dim interaction Drot
+    config = @strdict type dim interaction Drot mot
     savefig(plt, plotsdir(savename("D", config, "png")))
     savefig(plt, plotsdir(savename("D", config, "pdf")))
 end
+
+##
+for dim in [2], interaction in ["stick", "slide"], Drot in [1], mot in ["RunReverse"]
+    τ = range(0.1, 15, length=500)
+    plt = makeDplot_cylinders(df, dim, interaction, Drot, mot)
+    if Drot == 0
+        plot!(plt, ylims=(0.01, 0.45))
+    else
+        #τᵣ = 1 / (2*Drot)
+        #D = @. τ * τᵣ / (2 * dim * (τ + τᵣ))
+        D = @. (1/dim) * (1 / (2/τ + 2*Drot))
+        plot!(τ, D, lc=:green, lw=2, alpha=0.5, lab=false)
+        plot!(plt, ylims=(0.01, 0.175))
+    end
+    config = @strdict type dim interaction Drot mot
+    savefig(plt, plotsdir(savename("D", config, "png")))
+    savefig(plt, plotsdir(savename("D", config, "pdf")))
+end
+
+##
+for dim in [2], interaction in ["stick", "slide"], Drot in [1], mot in ["RunReverseFlick"]
+    τ = range(0.1, 15, length=500)
+    plt = makeDplot_cylinders(df, dim, interaction, Drot, mot)
+    if Drot == 0
+        plot!(plt, ylims=(0.01, 0.45))
+    else
+        τᵣ = 1 / (2*Drot)
+        D = @. 1 / (2*dim) * (1/τ + 2/τᵣ) / (1/τ + 1/τᵣ)^2
+        plot!(τ, D, lc=:green, lw=2, alpha=0.5, lab=false)
+        plot!(plt, ylims=(0.01, 0.175))
+    end
+    config = @strdict type dim interaction Drot mot
+    savefig(plt, plotsdir(savename("D", config, "png")))
+    savefig(plt, plotsdir(savename("D", config, "pdf")))
+end
+
+
