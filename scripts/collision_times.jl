48fb7668ac1d7b0f515ab9a6250c0d4638419b44
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-09-15 13:32:53 +0200

    evaluate times between collisions and times on surface

---
+++ scripts/collision_times.jl
@@ -0,0 +1,61 @@
+using DrWatson
+using Surfaces
+using JLD2, CSV
+using MicrobeAgents: vectorize_adf_measurement
+using DataFrames
+using GLMakie
+
+##
+function free_path(s::AbstractVector{Bool}; Δt=0.01)
+    ls = length(s)
+    iter = zip(view(s, 1:ls-1), view(s, 2:ls))
+    start = Int[0]
+    stop = Int[]
+    for (i,(a,b)) in enumerate(iter)
+        if b && !a
+            push!(stop, i)
+        elseif !b && a
+            push!(start, i)
+        end
+    end
+    if length(start) == length(stop)
+        return (stop .- start) .* Δt
+    else
+        return (stop .- start[1:end-1]) .* Δt
+    end
+end
+
+function bound_path(s::AbstractVector{Bool}; Δt=0.01)
+    ls = length(s)
+    iter = zip(view(s, 1:ls-1), view(s, 2:ls))
+    start = Int[]
+    stop = Int[]
+    for (i,(a,b)) in enumerate(iter)
+        if b && !a
+            push!(start, i)
+        elseif !b && a
+            push!(stop, i)
+        end
+    end
+    if length(start) == length(stop)
+        return (stop .- start) .* Δt
+    else
+        return (stop .- start[1:end-1]) .* Δt
+    end
+end
+
+##
+filenames = readdir(datadir("sims", "collision_times"); join=true)
+for fname in filenames
+    param = parse_savename(fname)[2]
+    f = jldopen(fname)
+    adf = f["adf"]
+    close(f)
+    s = vectorize_adf_measurement(adf, :is_stuck)
+    time_between_collisions = vcat(map(free_path, eachcol(s))...)
+    time_on_surface = vcat(map(bound_path, eachcol(s))...)
+    fout_collisions = replace(fname, "sims" => "proc", "traj" => "free", "jld2" => "csv")
+    CSV.write(fout_collisions, time_between_collisions |> Tables.table)
+    fout_surface = replace(fname, "sims" => "proc", "traj" => "bound", "jld2" => "csv")
+    CSV.write(fout_surface, time_on_surface |> Tables.table)
+end
---
+++ scripts/simulate_collision_times.jl
@@ -0,0 +1,51 @@
+## Libraries
+using Distributed
+@everywhere using DrWatson
+@everywhere @quickactivate :Surfaces
+@everywhere begin
+    using JLD2
+    using MicrobeAgents
+end
+
+## Routines for data production
+@everywhere function runsim(params)
+    @unpack dim, L, R, motilepattern, U, λ, Drot, interaction = params
+    model = initializemodel_cylinders(
+        dim, L, R,
+        SurfyMicrobe, motilepattern,
+        U, λ, Drot,
+        U, λ, 1.0, # match with bulk parameters
+        interaction == :stick ? stick! : slide!;
+        n = 100
+    )
+    adata = [:pos, :vel, :is_stuck]
+    nsteps = round(Int, 100/(λ*model.timestep))
+    adf, = run!(model, nsteps; adata)
+    @strdict adf
+end
+
+@everywhere function produce_data(config)
+    savedir = haskey(ENV, "SCRATCH") ? joinpath(ENV["SCRATCH"], "Surfaces") : datadir()
+    data = produce_or_load(
+        runsim, joinpath(savedir, "sims", "collision_times"), config;
+        prefix="traj", suffix="jld2",
+        tag=false, loadfile=false
+    )
+    data = nothing
+    GC.gc()
+    return nothing
+end
+
+## Setup parameters and run
+dim = [2]
+L = [1.0]
+R = [0.1, 0.15, 0.2, 0.25]
+motilepattern = [:RunTumble, :RunReverse, :RunReverseFlick]
+interaction = [:stick, :slide]
+U = [1.0]
+λ = exp10.(range(-1, 1, length=3))
+Drot = [0.0, 1.0]
+
+allparams = @strdict dim L R motilepattern interaction U λ Drot
+dicts = dict_list(allparams)
+pmap(produce_data, dicts)
