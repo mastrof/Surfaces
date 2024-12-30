b065375694aabe3e121ab68d3cd34056c576e75f
Riccardo Foffi <riccardo.foffi@protonmail.com>
2024-12-30 18:03:10 +0100

    update failed attachament parameters

--- scripts/simulate_cylinders_failedattach.jl
+++ scripts/simulate_cylinders_failedattach.jl
@@ -9,12 +9,12 @@ end
 
 ## Routines for data production
 @everywhere function runsim(params)
-    @unpack dim, L, R, motilepattern, U, λ, Drot, interaction = params
+    @unpack dim, L, R, motilepattern, U, λ, Drot, interaction, μ = params
     model = initializemodel_cylinders(
         dim, L, R,
         SurfyMicrobe, motilepattern,
         U, λ, Drot,
-        U, λ, 0.0, # match with bulk parameters
+        U, λ, μ,
         interaction == :stick ? stick! : slide!
     )
     adata = [:pos, :is_stuck]
@@ -39,13 +39,14 @@ end
 ## Setup parameters and run
 dim = [2]
 L = [1.0]
-R = [0.1, 0.15, 0.2, 0.25]
-motilepattern = [:RunTumble, :RunReverse, :RunReverseFlick]
+R = [0.1, 0.2]
+motilepattern = [:RunTumble]
 interaction = [:stick]
 U = [1.0]
 λ = exp10.(range(-1, 1, length=15))
 Drot = [0.1]
+μ = [0.5, 0.75]
 
-allparams = @strdict dim L R motilepattern interaction U λ Drot
+allparams = @strdict dim L R motilepattern interaction U λ Drot μ
 dicts = dict_list(allparams)
 pmap(produce_data, dicts)
